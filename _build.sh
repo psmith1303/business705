6a947af91466d84702436008a5f12370fa17d478
psmith1303 <psmith@consulting.co.nz>
2017-06-14 13:52:26 +1200

    Spin out the week-by-week material into its own chapter.

--- _build.sh
+++ _build.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-rm -f business705.Rmd
+BIB_MASTER=/home/psmith/NAS/Work/Research/Citations/bibs/psmithLibrary.bib
+BIB_LOCAL=book.bib
+TEMPFILE=temp.tmp
+#
+# Make the bibliography
+#
+cat *.Rmd > $TEMPFILE
+md2bib.py -f $TEMPFILE -o $BIB_LOCAL $BIB_MASTER
+touch $BIB_LOCAL
+
+rm -f business705.Rmd $TEMPFILE
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
